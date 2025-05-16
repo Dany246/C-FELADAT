9a0887497f2492192b482fb787eff114ccb891b7
Dany246 <145447252+Dany246@users.noreply.github.com>
2025-05-16 10:36:59 +0200

    Add files via upload

---
+++ szallas03/szallascon/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szallascon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Szallas> szallasok = Szallas.Beolvas("szallas2022.csv");
+
+            Console.WriteLine("4.feladat");
+            Console.WriteLine($"Az összes szálláshelyek száma: {szallasok.Count} darab");
+
+            int agyak = 0;
+            int aktiv = 0;
+
+            foreach (Szallas szallas in szallasok)
+            {
+                if (szallas.Statusz)
+                {
+                    aktiv++;
+                    agyak += szallas.AgySzam;
+                }
+            }
+            Console.WriteLine("5.feladat");
+
+            double atlag = (double)agyak / (double)aktiv; 
+
+            Console.WriteLine($"Átlagos ágyszám az aktív szálláshelyeken: {atlag:0.00}");
+
+            Console.WriteLine("6.feladat");
+            Console.WriteLine($"Szeghalom környéki aktív szállások:");
+          
+
+
+            Dictionary<string, int> telepulesek = new Dictionary<string, int>();
+
+            foreach (Szallas szallas in szallasok)
+            {
+                if (szallas.Statusz)
+                {
+                    if (telepulesek.ContainsKey(szallas.Telepules))
+                        telepulesek[szallas.Telepules]++;
+                    else
+                        telepulesek[szallas.Telepules] = 1;
+                }
+
+            }
+            foreach (var telepules in telepulesek)
+            {
+                Console.WriteLine($"\t{telepules.Key} : {telepules.Value} db");
+            }
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ szallas03/szallascon/szallas.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szallascon
+{
+    public class Szallas
+    {
+        public string Nev { get; private set; }
+        public string Cim { get; private set; }
+        public int Szobaszam { get; private set; } 
+        public int AgySzam { get; private set; } 
+        public string UzNeve { get; private set; } 
+        public string UzCime { get; private set; } 
+        public string UzTelefon { get; private set; } 
+        public bool Statusz { get; private set; } 
+        public string TevTipus { get; private set; }
+
+        public Szallas(string sor) 
+        {
+            string[] darabol = sor.Split(';');
+
+            Nev = darabol[0];
+            Cim = darabol[1];
+            Szobaszam = int.Parse(darabol[2]);
+            AgySzam = int.Parse(darabol[3]);
+            UzNeve = darabol[4];
+            UzCime = darabol[5];
+            UzTelefon = darabol[6];
+            Statusz = (darabol[7] == "Aktív");
+            TevTipus = darabol[8];  
+
+        }
+        public static List<Szallas> Beolvas(string file) 
+        {
+            List<Szallas> list = new List<Szallas>();
+
+            StreamReader sr = new StreamReader(file, Encoding.UTF8);
+
+            sr.ReadLine();
+            while (!sr.EndOfStream) 
+            {
+                string line = sr.ReadLine();
+                Szallas szallas = new Szallas(line);
+                list.Add(szallas);
+            }
+
+            
+            
+
+            sr.Close();
+
+            return list;
+        }
+
+        public string Telepules
+        {
+            get
+            {
+                return Cim.Split(' ')[1];
+            }
+        }
+    }
+}
+
